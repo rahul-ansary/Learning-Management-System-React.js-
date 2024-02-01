ca21b4e768330ad147028de3d83a9283285b4bd6
rahul-ansary <rahulansary38@gmail.com>
2024-02-01 22:55:07 +0530

    Added layout & homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-//CSS import
-import './App.css'
-//library imports
-import { Routes } from 'react-router-dom'
-import Footer from './Components/Footer'
+import './App.css';
+
+
+import { Route, Routes } from 'react-router-dom';
+
+
+import HomePage from './Pages/HomePage';
+// import Daisyui from './Components/Daisyui';
 
 function App() {
 
   return (
-      <>
-      {/* <Routes>
-      lms
-      </Routes> */}
-      
-      </>
+    <>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} ></Route>
+
+      </Routes>
+    </>
   )
 }
-
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
 }
-
